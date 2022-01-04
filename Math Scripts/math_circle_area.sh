d6700ddf7b708f34fa69e0557bdefa63f1649e8f
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2022-01-04 11:15:58 -0300

    Adding math and problem solving scripts

---
+++ Math Scripts/math_circle_area.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+read -p "Enter the circle's radius in square meters: " CIRCLE_RAY
+
+CIRCLE_AREA = $CIRCLE_RAY * $CIRCLE_RAY * 3.14159
+
+echo "{$CIRCLE_RAY} is the circle's radius in square meters."
+
+
---
+++ Problem Solving Scripts/basic_investment_calc.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+read -p "What currency are you using? (USD/BRL) " WHAT_CURRENCY
+read -p "What is the income percentage per year? " INCOME_PER
+read -p "See the projection until: (ex: 5) " INVESTMENT_UNTIL
+read -p "How much do you want to invest? " HOW_MUCH
+
+FINAL_INCOME = ($INCOME_PER * $HOW_MUCH) / 100
+TOTAL_INCOME = $FINAL_INCOME + $HOW_MUCH
+
+FINAL_INCOME_PRINTABLE = $TOTAL_INCOME * $NVESTMENT_UNTIL
+
+echo "Your final income in {$INVESTMENT_UNTIL} years is {$FINAL_INCOME_PRINTABLE} $WHAT_CURRENCY"
+
+# For loop to know how much you will have in {$INVESTMENT_UNTIL} years.
+for EACH_YEAR_INCOME in $(seq 1 $INVESTMENT_UNTIL)
+  do
+    TOT_INC = $TOTAL_INCOME * $EACH_YEAR_INCOME
+    echo "In $EACH_YEAR_INCOME year(s) you will have $TOT_INC $WHAT_CURRENCY"
+done
