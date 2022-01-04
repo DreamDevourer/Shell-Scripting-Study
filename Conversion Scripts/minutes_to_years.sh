ed8fb09c8f5d3c703fb4b3b2f84e900d8f2b8bb5
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2022-01-04 11:47:41 -0300

    Adding conversion script of minutes to years and fixing math circle area script

---
+++ Conversion Scripts/minutes_to_years.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# 24785089
+read -p "Type the minutes that you want to convert to yaers: " MINUTES
+echo "$MINUTES minutes is $((MINUTES/60/24/365)) years"
--- Math Scripts/math_circle_area.sh
+++ Math Scripts/math_circle_area.sh
@@ -2,8 +2,9 @@
 
 read -p "Enter the circle's radius in square meters: " CIRCLE_RAY
 
-CIRCLE_AREA = $CIRCLE_RAY * $CIRCLE_RAY * 3.14159
+CIRCLE_AREA=$((CIRCLE_RAY*CIRCLE_RAY))
+CIRCLE_FINAL=$((CIRCLE_AREA*3))
 
-echo "{$CIRCLE_RAY} is the circle's radius in square meters."
+echo "$CIRCLE_FINAL is the circle's radius in square meters."
 
 
